57b877f203ccd1f1a148df04263545ad8030a487
SakshhamTheCoder <sakshhamtg@gmail.com>
2026-07-08 23:25:35 +0530

    add --version flag

--- cmd/adbt/main.go
+++ cmd/adbt/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -11,7 +12,23 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
+// Populated by goreleaser at build time via -X ldflags.
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.BoolVar(showVersion, "v", false, "print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("adbt %s (commit %s, built %s)\n", version, commit, date)
+		return
+	}
+
 	if err := adb.EnsureAvailable(); err != nil {
 		fmt.Fprintln(os.Stderr, "adb not found in PATH.")
 		fmt.Fprintln(os.Stderr, "Install Android platform-tools and try again:")
